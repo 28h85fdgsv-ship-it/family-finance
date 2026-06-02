c200fd9c2c2b1b2537736f4649fa387de421cf6c
Shlomi Shemtov <shlomi.shemtov7@gmail.com>
2026-06-02 22:26:14 +0300

    SW פשוט - ללא יירוט בקשות

--- sw.js
+++ sw.js
@@ -1,17 +1,8 @@
-// shem-tov-v12 - force refresh all clients
 self.addEventListener('install', () => self.skipWaiting());
-
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
       .then(() => self.clients.claim())
-      .then(() => self.clients.matchAll({ type: 'window' }))
-      .then(clients => clients.forEach(c => c.postMessage({ type: 'RELOAD' })))
+      .then(() => self.clients.matchAll({type:'window'}).then(cs => cs.forEach(c => c.postMessage({type:'RELOAD'}))))
   );
-});
-
-self.addEventListener('fetch', e => {
-  if (!e.request.url.startsWith(self.location.origin)) return;
-  e.respondWith(fetch(e.request));
 });
